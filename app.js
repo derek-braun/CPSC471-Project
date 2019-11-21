2c75ec03dcab500ba5e3203bd83baf4c2be6177d
Derek Braun <derek.braun.99@gmail.com>
2019-11-21 11:34:43 -0700

    Add signup page, add body-parser

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
+//Establish database connection on server side
 var connection = mysql.createConnection({
     host    : 'localhost',
     user    : 'username',
@@ -11,10 +14,15 @@ var connection = mysql.createConnection({
     database: 'cpsc471project'
 });
 
+//Redirect main page to login
 app.get("/", function (req, res){
     //redirect to login
     res.redirect("/login");
 });
+
+app.get("/signup", function (req, res){
+    res.render("signup");
+})
 
 app.get("/login", function (req, res){
     // var q = "SELECT * FROM users";
@@ -22,9 +30,11 @@ app.get("/login", function (req, res){
     //     if(err) throw err;
     //     console.log(results);
     // })
-    res.render("login");
+    var bar = "Hello World";
+    res.render("login", {foo: bar});
 });
 
+// Prevent internal page access without login
 // app.use(function(req, res, next) {
 //     if(!req.session.accessToken) {
 //         res.redirect("/");
@@ -37,6 +47,7 @@ app.get("/other", function (req, res){
     res.send("You've reached a different page");
 });
 
+//Create server, wait for requests
 app.listen(8080, function(){
     console.log("Server Running on Port 8080");
 });
