d452ddbc50c46bebe50cbd47c71aad6905f6e438
Derek Braun <derek.braun.99@gmail.com>
2019-11-26 17:07:28 -0700

    Add login session and main navbar

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var app = express();
+var session = require('express-session');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,6 +21,13 @@ connection.connect((err) => {
     console.log('Connected!');
 });
 
+//use sessions for tracking logins
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false
+}));
+
 //Redirect main page to login
 app.get("/", function (req, res){
     //redirect to login
@@ -34,10 +42,6 @@ app.get("/login", function (req, res){
     res.render("login");
 });
 
-app.get("/search", function(req, res){
-    res.render("search");
-})
-
 app.post("/signup", function(req, res){
     var i = "INSERT INTO Users VALUES(?, ?);"
     connection.query(i, [req.body.username, req.body.password], function(err, results){
@@ -50,20 +54,33 @@ app.post("/login", function (req, res){
     var q = "SELECT * FROM Users WHERE Uname = ? AND Pass = ?;"
     connection.query(q, [req.body.username, req.body.password], function(err, results){
         if(err) console.log("An error has occurred");
-        if(results.length == 0) return res.redirect("/login");
-        else if(results.length == 1) return res.redirect("/search");
+        if(results.length == 0){
+            return res.redirect("/login");
+        }
+        else if(results.length == 1) {
+            req.session.userId = results[0].Uname;
+            return res.redirect("/search");
+        }
         console.log(results);
     })
 })
 
 // Prevent internal page access without login
-// app.use(function(req, res, next) {
-//     if(!req.session.accessToken) {
-//         res.redirect("/");
-//     } else {
-//         next();
-//     }
-// });
+app.use(function(req, res, next) {
+    if(req.session && req.session.userId) {
+        next();
+    } else {
+        res.redirect("/");
+    }
+});
+
+app.get("/search", function(req, res){
+    res.render("search");
+})
+
+app.get("/profile", function(req, res){
+    res.render("profile");
+})
 
 app.get("/other", function (req, res){
     res.send("You've reached a different page");
